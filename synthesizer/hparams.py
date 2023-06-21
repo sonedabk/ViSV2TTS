d25c8e7f79f23dc42d5c20cc530497380d9038f0
tvcuong98 <unikool8989@gmail.com>
2023-06-21 16:49:07 +0700

    Train single gpu

--- synthesizer/hparams.py
+++ synthesizer/hparams.py
@@ -49,12 +49,12 @@ hparams = HParams(
                                                     # frame that has all values < -3.4
 
         ### Tacotron Training
-        tts_schedule = [(2,  1e-3,  20_000,  2),   # Progressive training schedule
-                        (2,  5e-4,  40_000,  2),   # (r, lr, step, batch_size)
-                        (2,  2e-4,  80_000,  2),   #
-                        (2,  1e-4, 160_000,  2),   # r = reduction factor (# of mel frames
-                        (2,  3e-5, 320_000,  2),   #     synthesized for each decoder iteration)
-                        (2,  1e-5, 640_000,  2)],  # lr = learning rate
+        tts_schedule = [(2,  1e-3,  20_000,  1),   # Progressive training schedule
+                        (2,  5e-4,  40_000,  1),   # (r, lr, step, batch_size)
+                        (2,  2e-4,  80_000,  1),   #
+                        (2,  1e-4, 160_000,  1),   # r = reduction factor (# of mel frames
+                        (2,  3e-5, 320_000,  1),   #     synthesized for each decoder iteration)
+                        (2,  1e-5, 640_000,  1)],  # lr = learning rate
 
         tts_clip_grad_norm = 1.0,                   # clips the gradient norm to prevent explosion - set to None if not needed
         tts_eval_interval = 500,                    # Number of steps between model evaluation (sample generation)
